e497ea0bbe3e7091c0c9afe1e0fb6c07b9c95beb
Leo Sharif <54294187+Realaiz@users.noreply.github.com>
2024-05-25 00:17:15 +1000

    zzz

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -27,11 +27,14 @@ export default function NavLinks() {
   return (
     <>
       {links.map((link) => {
+                    console.log(`Type of link.href: ${typeof link.href}`); // Log the type of href
+            console.log(`Value of link.href: ${link.href}`); // Log the value of href
         const LinkIcon = link.icon;
         return (
           <Link
             key={link.name}
             href={link.href}
+
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
