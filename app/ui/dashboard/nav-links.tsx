928d9c2ec65dd635c0a666d3bd21a76dc4636c38
Leo Sharif <54294187+Realaiz@users.noreply.github.com>
2024-05-28 20:37:53 +1000

    ..

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -11,11 +11,11 @@ import clsx from 'clsx';
 
 
 
-const links  {
+const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon},
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-}
+];
 export default function NavLinks() {
   const pathname = usePathname();
 
